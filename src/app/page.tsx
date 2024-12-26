75d2c6f698259ab58da2b930430695682afc3cd7
jong-un <36259838+zwong91@users.noreply.github.com>
2024-12-26 21:43:18 +0800

    更新 page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,6 +33,7 @@ export default function Home() {
       if (isPlayingAudio) {
         setIsPlayingAudio(false);
       }
+      audioBufferQueue = []
     },
 
     playNewAudio: async (audioBlob: Blob) => {
